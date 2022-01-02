592231a2e80b43870f117be85033c767f99b845e
vi <tuanm.dev@gmail.com>
2022-01-02 15:11:33 +0700

    create base

---
+++ src/main/java/dev/tuanm/tuancerts/issuer/IssuerApplication.java
@@ -0,0 +1,13 @@
+package dev.tuanm.tuancerts.issuer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tuanm/tuancerts/issuer/IssuerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tuanm.tuancerts.issuer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
